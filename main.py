b94fc3bf52f7758f66a8529afdd118a344330d29
aksenianets <aksenianets@gmail.com>
2024-04-07 14:12:49 +0300

    начало работы с html

--- main.py
+++ main.py
@@ -19,6 +19,85 @@ def index():
     return render_template("index.html", headline="Главная")
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template(
+            "login.html", message="Неправильный логин или пароль", form=form
+        )
+    return render_template("login.html", title="Авторизация", form=form)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template(
+                "register.html",
+                title="Регистрация",
+                form=form,
+                message="Пароли не совпадают",
+            )
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template(
+                "register.html",
+                title="Регистрация",
+                form=form,
+                message="Такой пользователь уже есть",
+            )
+        user = User(name=form.name.data, email=form.email.data, about=form.about.data)
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        login_user(user, remember=True)
+        return redirect("/")
+    return render_template("register.html", title="Регистрация", form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+
+    return redirect("/")
+
+
+@app.route("/profile/<int:user_id>")
+def profile(user_id):
+
+    db_sess = db_session.create_session()
+    # answers = (
+    #     db_sess.query(User)
+    #     .filter(User.)
+    #     .all()
+    # )
+    # wrong_answers = (
+    #     db_sess.query(User)
+    #     .filter(User.)
+    #     .all()
+    # )
+
+    # return render_template(
+    #     "profile.html",
+    #     user=load_user(user_id),
+    #     answers=answers,
+    #     wrong_answers=wrong_answers,
+    # )
+
+
 @app.errorhandler(404)
 def not_found(error):
     return """
